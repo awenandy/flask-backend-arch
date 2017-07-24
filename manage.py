ae2c23e4186765dc180e4646d0f177efd13ce240
May's Father <shewenkui@shewenkuideMacBook-Pro.local>
2017-07-24 16:37:15 +0800

    modify managy.py

--- manage.py
+++ manage.py
@@ -1,1 +1,14 @@
-#!/usr/bin/env pythonfrom main.wsgi import appfrom main.wsgi import init_appfrom main.database import dbfrom main.common.userimpl import new_userfrom main.testdb import testdbinit_app(db)new_user()testdb.checkpassword()if __name__ == '__main__':     app.run(port=8003)
+#!/usr/bin/env python
+
+from main.wsgi import app
+from main.wsgi import init_app
+from main.database import db
+from main.common.userimpl import new_user
+from main.testdb import testdb
+
+init_app(db)
+new_user()
+testdb.checkpassword()
+
+if __name__ == '__main__':
+     app.run(port=8003)
