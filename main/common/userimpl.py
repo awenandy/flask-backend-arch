eba56492bb2ef74aee3b08d2fc3e4fd20310c647
Wang Haiqing <krman@163.com>
2017-08-09 16:32:01 +0800

    只添加一次用户到数据库

--- main/common/userimpl.py
+++ main/common/userimpl.py
@@ -2,8 +2,12 @@ from main.models.user import User
 from main.database import db
 
 def new_user():
-    user = User(username='flask')
-    user.hash_password('ssan')
-    db.session.add(user)
-    db.session.commit()
+    username = 'flask'
+    password = 'ssan'
+    query_all = db.session.query(User).filter(User.username == username).all()
+    if not query_all:
+        user = User(username=username)
+        user.hash_password(password)
+        db.session.add(user)
+        db.session.commit()
     return True
